4d80c8023590b7f72f79c0ea89a6d034df25286f
Abby <smitha9294@gmail.com>
2021-06-10 22:58:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useEffect, useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [creatureData, setCreatureData] = useState([])
+	const [searchTerm, setSearchTerm] = useState('')
+
+	useEffect(() => {
+		if (searchTerm.length >= 2) {
+			fetch(
+				`https://us.api.blizzard.com/data/wow/search/creature?namespace=static-us&name.en_US=${searchTerm}&orderby=id&_page=1&access_token=USsnG0WSVQC6vzdh93pY4YbhKPBgTYfHhY`
+			)
+				.then((response) => {
+					return response.json()
+				})
+				.then((creatureDataResponse) => {
+					if (creatureDataResponse.results.length > 0) {
+						//setCreatureData(response.results)
+							creatureDataResponse.results.forEach((result) => {				fetch(
+								`https://us.api.blizzard.com/data/wow/media/creature-display/${result.data.id}?namespace=static-classic-us&locale=en_US&access_token=USsnG0WSVQC6vzdh93pY4YbhKPBgTYfHhY`
+							)
+								.then((response) => {
+									return response.json()
+								})
+								.then((imgData) => {
+									if (imgData.assets) {
+										result.data.image = imgData.assets[0].value
+									}
+								})
+						})
+						setCreatureData(creatureDataResponse.results)
+					}
+				})
+		}
+	}, [searchTerm])
+
+	console.log(creatureData)
+
+	return (
+		<div className="App">
+			<input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+			<div className="resultsDiv">
+				{
+					creatureData.map((creature) => {
+						return (
+							<div>
+								{creature.data.name.en_US}
+							</div>
+						)
+					})
+				}
+			</div>
+		</div>
+	);
 }
 
 export default App;
